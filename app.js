adc1fc689229f856863d1099a600a8137e787def
Xchel Uriel Pérez González <L20290857@cdguzman.tecnm.mx>
2026-01-08 16:19:37 -0600

    Modificacion de app y se agregaron dependencias en el paquete

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 import express from 'express';
-import  {PORT}  from './configurations.js';
+import db from './db/db.js'; 
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 const app = express();
 
-app.get('/', (req, res) =>{
-    res.send("Hola mundo")
-})
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-app.listen(PORT, () =>{
-    console.log("Servidor abierto")
-})
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('login');
+});
+
+app.listen(3000, () => {
+  console.log('Servidor iniciado');
+});
