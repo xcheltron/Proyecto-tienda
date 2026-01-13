7bf1a3ea52b4b1f074cc78ef90f8d11a31e0b185
Xchel Uriel Pérez González <L20290857@cdguzman.tecnm.mx>
2026-01-13 12:35:32 -0600

    Se agrego una configuracion extra de los salt-rounds

--- configurations.js
+++ configurations.js
@@ -7,3 +7,4 @@ export const DB_USER = process.env.DB_USER;
 export const DB_HOST = process.env.DB_HOST;
 export const DB_DATA = process.env.DB_DATA;
 export const DB_PASS = process.env.DB_PASS;
+export const SALT_ROUNDS = process.env.SALT_ROUNDS;
