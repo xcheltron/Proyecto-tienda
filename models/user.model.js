9df3f80a460d94165271a0b84c8da0bf77d07201
Xchel Uriel Pérez González <L20290857@cdguzman.tecnm.mx>
2026-01-13 12:22:07 -0600

    Se agrego el modelo de registrar usuario

---
+++ models/user.model.js
@@ -0,0 +1,15 @@
+//Todos los modelos de la base de datos relacionados con el usario
+//Registrar
+//Modificar
+//Eliminar
+//Dar inicio de sesion
+import connection from '../db/db.js';
+
+export const registerUser = (user, passwordHash, rol) =>{
+    return new Promise ((resolve, reject) =>{
+        connection.query('CALL Insertar_Usuario (?,?,?)',[user, passwordHash, rol], (err, results) =>{
+            if (err) reject(err)
+                else resolve(results)
+        })
+    })
+}
