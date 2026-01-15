c54651fdbbe183f80e5e22083c8c2e302dc1cfc3
Xchel Uriel Pérez González <L20290857@cdguzman.tecnm.mx>
2026-01-15 09:44:43 -0600

    Cambios menores a app

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 import express from 'express';
+import session from 'express-session';
 import {PORT} from './configurations.js'
+import userRoutes from './routes/user.routes.js'
+import authRoutes from './routes/auth.routes.js'
 import db from './db/db.js'
 
 const app = express();
@@ -7,12 +10,21 @@ const app = express();
 //cargar las vistas
 app.set('view engine', 'ejs');
 
+app.use(session({
+  secret: 'mi_secreto_super_seguro',
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    secure: false, // true solo si usas HTTPS
+  }
+}));
+
 app.use(express.static('public'))
 app.use(express.json());
+app.use(express.urlencoded({extended: true}))
 
-app.get('/', (req, res) => {
-  res.render('Register');
-});
+app.use('/users', userRoutes)
+app.use('/', authRoutes)
 
 app.listen(PORT, () => {
   console.log(`Servidor iniciando en el puerto: ${PORT}`);
