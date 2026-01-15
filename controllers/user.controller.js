ac4dc279b1567f22a015390afe213256a371e2a5
Xchel Uriel Pérez González <L20290857@cdguzman.tecnm.mx>
2026-01-15 09:39:23 -0600

    Se cambio la funcionalidad de login y registrar

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,14 +1,51 @@
-import { registerUser } from "../models/user.model.js";
+import { registerUser, loigIn } from "../models/user.model.js";
 import { SALT_ROUNDS } from "../configurations.js";
 import bcrypt from 'bcrypt';
 
 export const registerUserController = async (req, res) => {
     const {user, password, rol} = req.body;
-    const passHash = await bcrypt.hashSync(password, SALT_ROUNDS);
+    
+    const passHash = await bcrypt.hash(password, parseInt(SALT_ROUNDS));
     try {
-        const results = registerUser(user, passHash, rol);
+        const results = await registerUser(user, passHash, rol);
         console.log(results);
-        res.render(HomePage)
+        
+        req.session.user = {
+            name: user,
+            rol: rol
+        };
+
+        if (results[0][0].mensaje === "ERROR OCURRIDO"){
+            res.redirect('register', {error: "Ocurrio un error"})
+        }
+
+        res.redirect('/home')
+
+    } catch (error) {
+        res.send(error)
+    }
+}
+
+export const logInController = async (req, res) => {
+    const {user, password} = req.body;
+    try {
+        const usuario = loigIn(user);
+        
+        if(!usuario){
+            return res.render('login', {error: 'Usuario no existe'})
+        }
+
+        const match = await bcrypt.compare(password, usuario.passwordHash)
+        if (!match){
+            return res.render('login', {error: 'Contraseña incorrecta'})
+        }
+
+        req.session.user = {
+            name: usuario.usuario,
+            rol: usuario.rol
+        };
+
+        res.redirect('/home');
     } catch (error) {
         res.send(error)
     }
