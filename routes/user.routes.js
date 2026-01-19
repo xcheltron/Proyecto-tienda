fac14dfa2736c2c88b2b738252e61443b4b84b8a
Xchel Uriel Pérez González <L20290857@cdguzman.tecnm.mx>
2026-01-19 11:08:00 -0600

    Se agrego la funcion de cierre de sesión

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -9,11 +9,16 @@ router.get('/login', (req, res) => {
     res.render('login',);
 });
 
-router.get('/register', (req, res) => {
+router.get('/register', isAuth, (req, res) => {
     res.render('Register',);
 });
 
-router.post('/register', registerUserController)
-router.post('/login', logInController)
+router.get('/logout', isAuth, (req,res) => {
+    req.session.destroy();
+    res.redirect('login');
+})
+
+router.post('/register', isAuth, registerUserController)
+router.post('/login', isAuth, logInController)
 
 export default router;
